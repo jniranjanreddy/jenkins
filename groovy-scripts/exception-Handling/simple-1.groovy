9fdc9194e2c3b51bb3dcf8f8f6353f7569807cc9
niru <you@example.com>
2022-01-12 11:41:24 -0500

    ;k

--- groovy-scripts/exception-Handling/simple-1.groovy
+++ groovy-scripts/exception-Handling/simple-1.groovy
@@ -1,9 +1,11 @@
 #!/usr/bin/env groovy
 try {
-a = "Rama"
-    println(a)
-} catch(groovy.lang.MissingPropertyException ex ) {
-    println("Entering to exception")
-    println("Sri Sita")
-    //println(a)
+    a = "Rama"
+    println(b)
+} catch(groovy.lang.MissingPropertyException cd ) {
+    def b = "Sita"
+    println("Entering to exception!")
+    println(b + "" + "and" + a)
+    println(cd)
 }
+println("I am out of block")
